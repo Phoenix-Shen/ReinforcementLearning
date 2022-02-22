c9147940fe709769553fd5901db2df6122f583cc
whussk <2250257231@qq.com>
2022-02-23 02:13:31 +0800

    feat: :sparkles: sgd

--- PytorchAndPythonLearning/Pytorch/pltutils.py
+++ PytorchAndPythonLearning/Pytorch/pltutils.py
@@ -153,3 +153,28 @@ def data_iter(batch_size: int, features: torch.Tensor, labels: torch.Tensor):
 
 def annotate(text, xy, xytext):
     plt.gca().annotate(text, xy, xytext, arrowprops=dict(arrowstyle="->"))
+
+def train_2d(trainer, steps=20, f_grad=None):  # @save
+    """用定制的训练机优化2D目标函数"""
+    # s1和s2是稍后将使用的内部状态变量
+    x1, x2, s1, s2 = -5, -2, 0, 0
+    results = [(x1, x2)]
+    for i in range(steps):
+        if f_grad:
+            x1, x2, s1, s2 = trainer(x1, x2, s1, s2, f_grad)
+        else:
+            x1, x2, s1, s2 = trainer(x1, x2, s1, s2)
+        results.append((x1, x2))
+    print(f'epoch {i + 1}, x1: {float(x1):f}, x2: {float(x2):f}')
+    return results
+
+
+def show_trace_2d(f, results):  # @save
+    """显示优化过程中2D变量的轨迹"""
+    set_figsize()
+    plt.plot(*zip(*results), '-o', color='#ff7f0e')
+    x1, x2 = torch.meshgrid(torch.arange(-5.5, 1.0, 0.1),
+                            torch.arange(-3.0, 1.0, 0.1),indexing="xy")
+    plt.contour(x1, x2, f(x1, x2), colors='#1f77b4')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
