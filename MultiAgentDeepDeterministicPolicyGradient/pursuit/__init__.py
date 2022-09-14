7a25bae88813023414fdcd101293cd1d6e800ffd
shikunshen <2250257231@qq.com>
2022-09-14 19:08:26 +0800

    revert: :poop: Deleted the code in MADDPG because the Env was too old and didn't suit for python 3

--- MultiAgentDeepDeterministicPolicyGradient/pursuit/__init__.py
+++
@@ -1,4 +0,0 @@
-from .pursuit_evade import PursuitEvade
-from .utils import RandomPolicy, SingleActionPolicy, TwoDMaps
-from .waterworld import MAWaterWorld
-from .waterworld_modified import MAWaterWorld_mod
--- MultiAgentDeepDeterministicPolicyGradient/pursuit/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Multi-agent utilities
-"""
-
-from .AgentLayer import AgentLayer
-from .Controllers import *
-from .DiscreteAgent import DiscreteAgent
-from .TwoDMaps import *
-from .agent_utils import *
